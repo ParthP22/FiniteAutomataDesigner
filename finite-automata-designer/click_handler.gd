3a74fa15be55e1004964d5cefc5c8b413d670dcb
Collyz <m.mowla03151@gmail.com>
2025-02-05 21:08:10 -0500

    text field and spawn fa one at a time until labeled

--- finite-automata-designer/click_handler.gd
+++ finite-automata-designer/click_handler.gd
@@ -1,5 +1,7 @@
 extends Area2D
 var _fa_scene = preload("res://fa_node.tscn") # Load once for efficiency
+var _current_selected_node = null
+var _spawned_and_unlabeled = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,14 +12,17 @@ func _input(event):
 		spawn_fa_node(get_global_mouse_position())
 
 func spawn_fa_node(mousePos: Vector2):
-	print("Node clicked! Spawning FA!")
-	# instantiate the fa_node scene
-	var fa_instance = _fa_scene.instantiate()
-	
-	# set position to mouse
-	fa_instance.position = mousePos
-	#actually spawn the node
-	get_parent().add_child(fa_instance)
+	if !_spawned_and_unlabeled:
+		print("Node clicked! Spawning FA!")
+		# instantiate the fa_node scene
+		var fa_instance = _fa_scene.instantiate()
+		# set position to mouse
+		fa_instance.position = mousePos
+		#actually spawn the node
+		get_parent().add_child(fa_instance)
+		_spawned_and_unlabeled = true
+	else:
+		print("current spawned fa missing label")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
