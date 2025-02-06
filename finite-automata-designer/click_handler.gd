5dca0b52d3662d7493fd0848335d10758a2af213
Collyz <m.mowla03151@gmail.com>
2025-02-05 21:01:15 -0500

    spawn fa_node.tscn at mouse position

--- finite-automata-designer/click_handler.gd
+++ finite-automata-designer/click_handler.gd
@@ -1,16 +1,21 @@
 extends Area2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		_on_click()
+		spawn_fa_node(get_global_mouse_position())
 
-func _on_click():
-	print("Node clicked!")
+func spawn_fa_node(mousePos: Vector2):
+	print("Node clicked! Spawning FA!")
+	
+	var fa_scene = load("res://fa_node.tscn")
+	var fa_instance = fa_scene.instantiate()
+	fa_instance.position = mousePos
+	get_parent().add_child(fa_instance)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
