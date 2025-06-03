490218adb7e5bd5b324b120c06e187c8a2a00abd
ParthP22 <parthpatel0422@gmail.com>
2025-06-03 17:13:17 -0400

    added a back button to the bottom left corner of the page to return to the homepage

--- finite-automata-designer/src/app/dfa/page.tsx
+++ finite-automata-designer/src/app/dfa/page.tsx
@@ -1,7 +1,29 @@
-export default function DfaPage() {
+'use client';
+
+import Link from "next/link";
+
+
+export default function DFAPage() {
+
     return (
-      <main className="min-h-screen flex items-center justify-center bg-white">
-        <h1 className="text-4xl font-bold">This is the DFA page</h1>
+      <main className="min-h-screen bg-blue-100 flex flex-col">
+        {/* Back Button to return to Home Page */}
+        <Link href="/" className="absolute bottom-4 left-4 px-6 py-3 bg-gray-700 text-white rounded hover:bg-black transition">
+            ← Back
+        </Link>
+
+        
+        
+        {/* DFA title at the top */}
+        <h1 className="relative text-5xl font-bold text-center mt-8 text-black">
+            <span className="relative z-10 drop-shadow-[0_0_1px_rgba(0,0,0,0.7)]">
+                Deterministic Finite Automata
+            </span>
+            <span className="absolute inset-x-0 top-0 h-8 bg-gradient-to-b from-white/30 via-white/10 to-transparent opacity-20 rounded pointer-events-none"></span>
+        </h1>
+
+        
       </main>
+      
     );
-  }
+}
