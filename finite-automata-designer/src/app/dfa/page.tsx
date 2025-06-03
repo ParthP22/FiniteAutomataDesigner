277c9f9b088e0b937b8996df858b3d906d862533
ParthP22 <parthpatel0422@gmail.com>
2025-06-03 16:23:00 -0400

    created a separate page to redirect to for DFA. Correctly linked to the DFA page from the homepage

---
+++ finite-automata-designer/src/app/dfa/page.tsx
@@ -0,0 +1,7 @@
+export default function DfaPage() {
+    return (
+      <main className="min-h-screen flex items-center justify-center bg-white">
+        <h1 className="text-4xl font-bold">This is the DFA page</h1>
+      </main>
+    );
+  }
