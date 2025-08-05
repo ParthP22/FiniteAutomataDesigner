befa97afe3b41441746c9983371cb020576dc8a8
ParthP22 <parthpatel0422@gmail.com>
2025-08-05 15:23:20 -0400

    feat: when registering a new user, the server now checks to see if the user already exists. If it does, then it throws a 409 error. Else, it'll add the user into the DB. Future improvements: check to see if the same email is already being used, because it currently only checks for userName

--- finite-automata-designer/src/pages/api/register-user.ts
+++ finite-automata-designer/src/pages/api/register-user.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
 import { ddbDocClient } from "@/lib/dynamodb"; // adjust path as needed
+import { TableNotFoundException } from "@aws-sdk/client-dynamodb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") return res.status(405).end();
@@ -8,12 +9,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { userID, name, email, password } = req.body;
 
   try {
-    const command = new PutCommand({
+    const getCommand = new GetCommand({
+        TableName: "Users",
+        Key:{
+            userID: userID,
+        }
+    });
+
+    const {Item} = await ddbDocClient.send(getCommand);
+
+    if(Item){
+        return res.status(409).json({ message: "User already exists!" });
+    }
+    
+
+    const putCommand = new PutCommand({
       TableName: "Users",
       Item: { userID, name, email, password },
     });
 
-    const result = await ddbDocClient.send(command);
+    const result = await ddbDocClient.send(putCommand);
     res.status(200).json({ success: true, result });
   } catch (err) {
     console.error("DynamoDB error:", err);
