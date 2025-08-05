68fb7693644c7a18b745de006670677711417722
ParthP22 <parthpatel0422@gmail.com>
2025-08-04 21:24:31 -0400

    feature: added DynamoDBAdapter to connect DynamoDB to NextAuth. It doesn't work as intended right now, but I'm sure we'll need it later when deploying it

--- finite-automata-designer/src/pages/api/auth/[...nextauth].ts
+++ finite-automata-designer/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,56 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
+// import CredentialsProvider from "next-auth/providers/credentials";
+import { DynamoDB, DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { ddbDocClient } from "@/lib/dynamodb";
+import { DynamoDBAdapter } from '@auth/dynamodb-adapter';
+import { adapter } from "next/dist/server/web/adapter";
+
+const config: DynamoDBClientConfig = {
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+  region: process.env.AWS_REGION!,
+}
+
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  }
+})
+
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [
+//     Google({
+//       authorization: {
+//         params: {
+//           prompt: "consent",
+//           access_type: "offline",
+//           response_type: "code",
+//         },
+//       },
+//     })
+//   ],
+// })
+
+// export const { handers, auth, signIn, signOut } = NextAuth({
+//   providers: [
+//     Google({
+//       authorization: {
+//         params: {
+//           prompt: "consent",
+//           access_type: "offline",
+//           response_type: "code",
+//         },
+//       },
+//     })
+//   ],
+//   adapter: DynamoDBAdapter(client),
+// })
 
 export const authOptions = {
   providers: [
@@ -11,7 +60,7 @@ export const authOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-
+  //adapter: DynamoDBAdapter(client),
   callbacks: {
     async signIn({ user, profile }: { user: any; profile?: any }) {
       try {
