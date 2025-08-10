8320473c03c8ea3a9c0105794697a49f76ef767f
ParthP22 <parthpatel0422@gmail.com>
2025-08-09 23:35:05 -0400

    refactor: moved all code related to the Circle class from dfaCanvas.ts into its own TS file

---
+++ finite-automata-designer/public/scripts/circle.ts
@@ -0,0 +1,63 @@
+import {Arrow} from "./arrow";
+import { selectedObj, nodeRadius, drawText } from "./draw";
+
+export var circles: Circle[] = [];
+
+export class Circle {
+  x: number;
+  y: number;
+  mouseOffsetX: number;
+  mouseOffsetY: number;
+  isAccept: boolean;
+  text: string;
+  outArrows: Arrow[];
+  loop: boolean;
+
+  constructor(x: number, y: number) {
+    this.x = x,
+    this.y = y;
+    this.mouseOffsetX = 0;
+    this.mouseOffsetY = 0;
+    this.isAccept = false;
+    this.text = '';
+    this.outArrows = [];
+    this.loop = false;
+  }
+
+  setMouseStart(x: number, y: number): void {
+    this.mouseOffsetX = this.x - x;
+    this.mouseOffsetY = this.y - y;
+  }
+
+  setAnchorPoint(x: number, y: number): void {
+    this.x = x + this.mouseOffsetX;
+    this.y = y + this.mouseOffsetY;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, nodeRadius, 0, 2 * Math.PI, false);
+    ctx.stroke();
+    drawText(ctx, this.text, this.x, this.y, null, selectedObj == this);
+
+    if (this.isAccept) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, nodeRadius - 5, 0, 2 * Math.PI, false);
+      ctx.stroke();
+    }
+  }
+
+  closestPointOnCircle(x: number, y: number) {
+    var dx = x - this.x;
+    var dy = y - this.y;
+    var scale = Math.sqrt(dx * dx + dy * dy);
+    return {
+      'x': this.x + dx * nodeRadius / scale,
+      'y': this.y + dy * nodeRadius / scale
+    }
+  }
+
+  containsPoint(x: number, y: number) {
+    return (x - this.x) * (x - this.x) + (y - this.y) * (y - this.y) < nodeRadius * nodeRadius;
+  }
+}
