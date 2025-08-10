0ceb67f724865c67809105344f4cc9439a79e50c
ParthP22 <parthpatel0422@gmail.com>
2025-08-09 23:42:03 -0400

    feat: these are the newly compiled JS files from the newly added TS files to help simplify the code. I will likely be compiling these TS files so that it compiles into one big JS file instead of these 7 separate JS files, since it'll make it easier for NextJS to interact with this one file only

---
+++ finite-automata-designer/public/scripts/draw.js
@@ -0,0 +1,39 @@
+export var nodeRadius = 30;
+export var selectedObj = null;
+export var snapToPadding = 10; // pixels
+export var hitTargetPadding = 6; // pixels
+// Creates subscript text to the input string using underscores before 0-9 as the regex
+function subscriptText(text) {
+    var subscriptText = text;
+    for (var i = 0; i < 10; i++) {
+        subscriptText = subscriptText.replace(new RegExp('_' + i, 'g'), String.fromCharCode(8320 + i));
+    }
+    return subscriptText;
+}
+export function drawText(ctx, originalText, x, y, angeOrNull, isSelected) {
+    ctx.font = '20px Times New Roman', 'serif';
+    var text = subscriptText(originalText);
+    var width = ctx.measureText(text).width;
+    x -= width / 2;
+    if (angeOrNull != null) {
+        var cos = Math.cos(angeOrNull);
+        var sin = Math.sin(angeOrNull);
+        var cornerPointX = (width / 2 + 5) * (cos > 0 ? 1 : -1);
+        var cornerPointY = (10 + 5) * (sin > 0 ? 1 : -1);
+        var slide = sin * Math.pow(Math.abs(sin), 40) * cornerPointX - cos * Math.pow(Math.abs(cos), 10) * cornerPointY;
+        x += cornerPointX - sin * slide;
+        y += cornerPointY + cos * slide;
+    }
+    x = Math.round(x);
+    y = Math.round(y);
+    ctx.fillText(text, x, y + 6);
+}
+export function drawArrow(ctx, x, y, angle) {
+    var dx = Math.cos(angle);
+    var dy = Math.sin(angle);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - 10 * dx + 5 * dy, y - 8 * dy - 5 * dx);
+    ctx.lineTo(x - 10 * dx - 5 * dy, y - 8 * dy + 5 * dx);
+    ctx.fill();
+}
