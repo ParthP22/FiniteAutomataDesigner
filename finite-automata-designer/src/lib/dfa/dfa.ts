e2993c534e579341610468389311e53817c51cc7
ParthP22 <parthpatel0422@gmail.com>
2025-08-10 15:27:40 -0400

    refactor: properly imported the necessary classes and arrays that will be used in the DFA algo

--- finite-automata-designer/src/lib/dfa/dfa.ts
+++ finite-automata-designer/src/lib/dfa/dfa.ts
@@ -1,52 +1,53 @@
-// // import { Circle, Arrow, circles, SelfArrow, alphabet } from "../../../public/scripts/dfaCanvas"
-// /// <reference path="../../../public/scripts/dfaCanvas.ts" />
+import { Circle, circles } from "../../../public/scripts/circle";
+import { alphabet } from "../../../public/scripts/dfaCanvas";
+import { Arrow } from "../../../public/scripts/arrow";
+import { SelfArrow } from "../../../public/scripts/SelfArrow";
 
+function transitionDeterminismCheck(circle: Circle, newTransition: string){
+    const transition = newTransition.trim().split(",");
 
-// function transitionDeterminismCheck(circle: Circle, newTransition: string){
-//     const transition = newTransition.trim().split(",");
+    circle.outArrows.forEach((arrow: Arrow) => {
+        const oldTransition = arrow.transition;
+        oldTransition.forEach((oldTransition: string) => {
+            transition.forEach((newTransition: string) => {
+                if(newTransition === oldTransition){
+                    return false;
+                }
+            });
+        });
+    });
+    return true;
+}
 
-//     circle.outArrows.forEach((arrow: Arrow) => {
-//         const oldTransition = arrow.transition;
-//         oldTransition.forEach((oldTransition: string) => {
-//             transition.forEach((newTransition: string) => {
-//                 if(newTransition === oldTransition){
-//                     return false;
-//                 }
-//             });
-//         });
-//     });
-//     return true;
-// }
+function inputDeterminismCheck(input: string){
+  for(let char of input){
+    if(!(char in alphabet)){
+      alert("Input contains " + char + ", which is not in the alphabet!");
+      return false;
+    }
+  }
 
-// function inputDeterminismCheck(input: string){
-//   for(let char of input){
-//     if(!(char in alphabet)){
-//       alert("Input contains " + char + ", which is not in the alphabet!");
-//       return false;
-//     }
-//   }
-
-//   for(let node of circles){
-//     const outArrows = node.outArrows;
-//     for(let char of alphabet){
-//       var exists: boolean = false;
-//       for(let arrow of outArrows){
-//         for(let transition of arrow.transition){
-//           if (char === transition){
-//             exists = true;
-//             break;
-//           }
-//           if(!(transition in alphabet)){
-//             alert("Transition " + transition + "for state " + node + " has not been defined in the alphabet");
-//             return false;
-//           }
-//         }
-//       }
-//       if(!exists){
-//         alert(char + " has not been implemented for this state: " + node + "; not all characters from alphabet were used");
-//         return false;
-//       }
-//     }
-//   }
-//   return true;
-// }
+  for(let node of circles){
+    const outArrows = node.outArrows;
+    for(let char of alphabet){
+      var exists: boolean = false;
+      for(let arrow of outArrows){
+        for(let transition of arrow.transition){
+          if (char === transition){
+            exists = true;
+            break;
+          }
+          if(!(transition in alphabet)){
+            alert("Transition " + transition + "for state " + node + " has not been defined in the alphabet");
+            return false;
+          }
+        }
+      }
+      if(!exists){
+        alert(char + " has not been implemented for this state: " + node + "; not all characters from alphabet were used");
+        return false;
+      }
+    }
+  }
+  return true;
+}
