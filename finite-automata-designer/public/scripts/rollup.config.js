b07a5e6b030ac38b52b5df7e5f63c2cb4e9cae02
ParthP22 <parthpatel0422@gmail.com>
2025-08-12 00:42:31 -0400

    refactor: the rollupJS file was moved into the project root a couple commits ago, but it has been moved into the public/scripts directory again

--- finite-automata-designer/public/scripts/rollup.config.js
+++ finite-automata-designer/public/scripts/rollup.config.js
@@ -1,3 +1,24 @@
+// import typescript from '@rollup/plugin-typescript';
+// import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+// export default {
+//   input: 'public/scripts/dfaCanvas.js', // Start with your TypeScript source file
+//   output: {
+//     file: 'public/scripts/dfaCanvas.js', // Output the bundled result here
+//     format: 'iife', // Immediately Invoked Function Expression (no modules)
+//     name: 'DfaCanvas', // Global variable name
+//     // globals: {
+//     //   // This makes your functions available globally
+//     //   'inputDeterminismCheck': 'inputDeterminismCheck',
+//     //   'transitionDeterminismCheck': 'transitionDeterminismCheck'
+//     // }
+//   },
+//   plugins: [
+//     nodeResolve(),
+//     typescript()
+//   ]
+// };
+
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
