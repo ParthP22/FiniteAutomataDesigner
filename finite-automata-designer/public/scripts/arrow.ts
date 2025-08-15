b1f351f69742282e8db09ba98e8a7c66628c27b6
ParthP22 <parthpatel0422@gmail.com>
2025-08-15 12:16:20 -0400

    refactor: deleted commented-out code in arrow.ts and also fixed some indentation

--- finite-automata-designer/public/scripts/arrow.ts
+++ finite-automata-designer/public/scripts/arrow.ts
@@ -4,24 +4,6 @@ import { EntryArrow } from "./EntryArrow";
 import { snapToPadding, nodeRadius, drawArrow, drawText, hitTargetPadding } from "./draw";
 
 export var arrows: (Arrow | SelfArrow | EntryArrow)[] = [];
-// export var lastEditedArrow: Arrow | SelfArrow | null = null;
-
-// export interface SharedState {
-//     lastEditedArrow: Arrow | SelfArrow | null;
-// }  
-
-// export var sharedState: {
-//     lastEditedArrow: Arrow | SelfArrow | null;
-// } = {
-//     lastEditedArrow: null,
-// };
-
-// export var sharedState = {
-//     {lastEditedArrow: lastEditedArrowType;} = {lastEditedArrow = null}
-// };
-
-
-
 
 function circleFromThreePoints(
   x1: number, 
@@ -68,11 +50,6 @@ export class Arrow {
   constructor(startCircle: Circle, endCircle: Circle) {
     this.startCircle = startCircle;
     this.endCircle = endCircle;
-
-    // startCircle.outArrows.add(this);
-    // console.log("outArrows size of start node after creating arrow: " + startCircle.outArrows.size);
-    // endCircle.inArrows.add(this);
-
     this.text = '';
     this.lineAngleAdjust = 0;
     // Make anchor point relative to the locations of start and end circles
@@ -164,33 +141,25 @@ export class Arrow {
         } else {
             drawArrow(ctx, pointInfo.endX, pointInfo.endY, Math.atan2(pointInfo.endY - pointInfo.startY, pointInfo.endX - pointInfo.startX));
         }
-    if (pointInfo.hasCircle) {
-      var startAngle = pointInfo.startAngle;
-      var endAngle = pointInfo.endAngle;
-      if (endAngle != null && startAngle != null&& endAngle < startAngle) {
-        endAngle += Math.PI * 2;
-      }
-      if (startAngle != null && endAngle != null && pointInfo.circleRadius != null){
-        var textAngle = (startAngle + endAngle) / 2 + (pointInfo.isReversed ? 1 : 0) * Math.PI;
-        var textX = pointInfo.circleX + pointInfo.circleRadius * Math.cos(textAngle);
-        var textY = pointInfo.circleY + pointInfo.circleRadius * Math.sin(textAngle);
-        drawText(ctx, this.text, textX, textY, textAngle);
-        // if(!transitionDeterminismCheck(this.startCircle,this.text)){
-        //   this.text = "";
-        //   alert("This transition fails the determinism check!");
-        // }
-      }
-    } else {
-      var textX = (pointInfo.startX + pointInfo.endX) / 2;
-      var textY = (pointInfo.startY + pointInfo.endY) / 2;
-      var textAngle = Math.atan2(pointInfo.endX - pointInfo.startX, pointInfo.startY - pointInfo.endY);
-      drawText(ctx, this.text, textX, textY, textAngle + this.lineAngleAdjust);
-      // if(!transitionDeterminismCheck(this.startCircle,this.text)){
-      //   this.text = "";
-      //   alert("This transition fails the determinism check!");
 
-      // }
-    }
+        if (pointInfo.hasCircle) {
+            var startAngle = pointInfo.startAngle;
+            var endAngle = pointInfo.endAngle;
+        if (endAngle != null && startAngle != null&& endAngle < startAngle) {
+            endAngle += Math.PI * 2;
+        }
+        if (startAngle != null && endAngle != null && pointInfo.circleRadius != null){
+            var textAngle = (startAngle + endAngle) / 2 + (pointInfo.isReversed ? 1 : 0) * Math.PI;
+            var textX = pointInfo.circleX + pointInfo.circleRadius * Math.cos(textAngle);
+            var textY = pointInfo.circleY + pointInfo.circleRadius * Math.sin(textAngle);
+            drawText(ctx, this.text, textX, textY, textAngle);
+        }
+        } else {
+            var textX = (pointInfo.startX + pointInfo.endX) / 2;
+            var textY = (pointInfo.startY + pointInfo.endY) / 2;
+            var textAngle = Math.atan2(pointInfo.endX - pointInfo.startX, pointInfo.startY - pointInfo.endY);
+            drawText(ctx, this.text, textX, textY, textAngle + this.lineAngleAdjust);
+        }
     }
 
     containsPoint(x: number, y: number) {
@@ -219,13 +188,13 @@ export class Arrow {
                 return (angle > startAngle && angle < endAngle);
             }
         } else {
-        var dx = stuff.endX - stuff.startX;
-        var dy = stuff.endY - stuff.startY;
-        var length = Math.sqrt(dx * dx + dy * dy);
-        var percent = (dx * (x - stuff.startX) + dy * (y - stuff.startY)) / (length * length);
-        var distance = (dx * (y - stuff.startY) - dy * (x - stuff.startX)) / length;
-        return (percent > 0 && percent < 1 && Math.abs(distance) < hitTargetPadding);
-      }
-      return false;
+            var dx = stuff.endX - stuff.startX;
+            var dy = stuff.endY - stuff.startY;
+            var length = Math.sqrt(dx * dx + dy * dy);
+            var percent = (dx * (x - stuff.startX) + dy * (y - stuff.startY)) / (length * length);
+            var distance = (dx * (y - stuff.startY) - dy * (x - stuff.startX)) / length;
+            return (percent > 0 && percent < 1 && Math.abs(distance) < hitTargetPadding);
+        }
+        return false;
     }
   }
