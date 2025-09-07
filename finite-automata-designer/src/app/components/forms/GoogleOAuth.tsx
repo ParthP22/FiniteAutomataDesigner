59730367e7676a388c8c694d6d25546bb0a41b4e
ParthP22 <parthpatel0422@gmail.com>
2025-09-07 16:30:16 -0400

    refactor: stylized the GoogleOAuth button so that it follows the same style as the website

--- finite-automata-designer/src/app/components/forms/GoogleOAuth.tsx
+++ finite-automata-designer/src/app/components/forms/GoogleOAuth.tsx
@@ -3,15 +3,17 @@ import { signinWithGoogle } from '@/lib/actions'
 import React from 'react'
 
 const GoogleOAuthForm = () => {
-    return (
-        <div>
-            <form>
-                <button className="btn" formAction={signinWithGoogle}>
-                    Sign in with Google
-                </button>
-            </form>
-        </div>
-    )
+  return (
+    <form className="flex justify-center">
+      <button
+        type="submit"
+        formAction={signinWithGoogle}
+        className="px-8 py-3 bg-gray-600 text-white text-lg rounded hover:bg-black hover:shadow-lg hover:scale-105 transition-transform duration-300"
+      >
+        Sign in with Google
+      </button>
+    </form>
+  )
 }
 
 export default GoogleOAuthForm
