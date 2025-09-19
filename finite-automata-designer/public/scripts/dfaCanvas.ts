14be74289eafcd23732f3c12e6ac5a8abe6b7334
Collyz <m.mowla03151@gmail.com>
2025-09-19 08:18:01 -0400

    Changed confirm prompt on import

--- finite-automata-designer/public/scripts/dfaCanvas.ts
+++ finite-automata-designer/public/scripts/dfaCanvas.ts
@@ -652,7 +652,7 @@ function attachWhenReady() {
             let data = inputTextArea.value;
             data = data.trim();
             if (data) {
-              if (confirm("Everything on canvas will be erased and lost! Proceed?")){
+              if (confirm("Everything on the canvas currently will be erased! Proceed with importing?")){
                 if (emptyDFA(canvas, arrows, circles)){
                   let SVGImporter = new Importer(circles, arrows, inputTextArea.value, drawRef);
                   SVGImporter.convert();
