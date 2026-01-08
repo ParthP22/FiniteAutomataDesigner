7c598f0fbe377f7e48330725080620a3bf194e30
ParthP22 <parthpatel0422@gmail.com>
2026-01-07 23:55:56 -0500

    feat: created the Trie class in the Trie.ts file

--- finite-automata-designer/src/lib/lexer/Trie.ts
+++ finite-automata-designer/src/lib/lexer/Trie.ts
@@ -4,3 +4,18 @@ export class TrieNode{
     children = new Map<string, TrieNode>;
     token?: Token;
 }
+
+export class Trie{
+    root = new TrieNode();
+
+    insert(word: string, token: Token){
+        let node = this.root;
+        for(const char of word){
+            if(!node.children.has(char)){
+                node.children.set(char, new TrieNode());
+            }
+            node = node.children.get(char)!;
+        }
+        node.token = token;
+    }
+}
