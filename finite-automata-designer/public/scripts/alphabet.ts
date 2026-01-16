027e0a1385a1a9b9acc291613330a9a98e9ba9d6
ParthP22 <parthpatel0422@gmail.com>
2026-01-16 14:36:19 -0500

    refactor: moved the transitionLabelInputValidator object into the alphabet.ts file. Also, when you set the alphabet, it also creates a new transitionLabelInputValidator, since we need to rebuild the prefix tree according to the new alphabet

--- finite-automata-designer/public/scripts/alphabet.ts
+++ finite-automata-designer/public/scripts/alphabet.ts
@@ -1,9 +1,17 @@
 // The alphabet defines every character that can be used in the DFA.
+
+import { TransitionLabelInputValidator } from "@/lib/validation/TransitionLabelInputValidator";
+
 // For easier usage, it has been defined as a Set.
 export var alphabet: Set<string> = new Set(["0","1"]);
+
+export var transitionLabelInputValidator: TransitionLabelInputValidator = new TransitionLabelInputValidator(alphabet); // Input validator for transition labels
+
 
 // Since the alphabet is being imported, it cannot be reassigned
 // directly. So, this is a setter method for it.
 export function setAlphabet(newAlphabet: Set<string>){
+    transitionLabelInputValidator = new TransitionLabelInputValidator(newAlphabet); // Input validator for transition labels
+
     alphabet = newAlphabet;
 }
