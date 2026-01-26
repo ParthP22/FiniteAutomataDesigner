93cf9d7a0f7f8ef80ca61056d99dc4a0ae80c559
ParthP22 <parthpatel0422@gmail.com>
2026-01-25 22:53:42 -0500

    feat: created epsilonSymbol string object and nfaTransitionSymbols, which is what is being passed into the transitionLabelInputValidator instead of the alphabet now. This is because the epsilon transition is not actually a part of the alphabet, but rather it is a symbol on its own. But, it is a valid symbol nonetheless and needs to be validated by the program

--- finite-automata-designer/src/lib/nfa/nfaTransitionSymbols.ts
+++ finite-automata-designer/src/lib/nfa/nfaTransitionSymbols.ts
@@ -4,14 +4,17 @@ import { TransitionLabelInputValidator } from "@/lib/validation/TransitionLabelI
 
 // For easier usage, it has been defined as a Set.
 export var alphabet: Set<string> = new Set(["0","1"]);
+export var epsilonSymbol: string = "\\epsilon"; // Symbol representing epsilon transitions
+export var nfaTransitionSymbols: Set<string> = new Set([...alphabet, epsilonSymbol]); // Separator for multiple transition symbols
 
-export var transitionLabelInputValidator: TransitionLabelInputValidator = new TransitionLabelInputValidator(alphabet); // Input validator for transition labels
+export var transitionLabelInputValidator: TransitionLabelInputValidator = new TransitionLabelInputValidator(nfaTransitionSymbols); // Input validator for transition labels
 
 
 // Since the alphabet is being imported, it cannot be reassigned
 // directly. So, this is a setter method for it.
 export function setAlphabet(newAlphabet: Set<string>){
-    transitionLabelInputValidator = new TransitionLabelInputValidator(newAlphabet); // Input validator for transition labels
-
+     // Input validator for transition labels
+    nfaTransitionSymbols = new Set([...newAlphabet, epsilonSymbol]); // Update separator set with new alphabet and epsilon symbol
     alphabet = newAlphabet;
+    transitionLabelInputValidator = new TransitionLabelInputValidator(nfaTransitionSymbols);
 }
