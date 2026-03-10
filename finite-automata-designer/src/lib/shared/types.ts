c4efde1a4ebfb5bf85394dcce585f05249667ca7
ParthP22 <parthpatel0422@gmail.com>
2026-03-10 15:26:57 -0400

    feat: added semicolons to the end of each attribute in the serialized state and transition types

--- finite-automata-designer/src/lib/shared/types.ts
+++ finite-automata-designer/src/lib/shared/types.ts
@@ -1,17 +1,17 @@
 export type SerializedState = {
     id: string;
-    x: number
-    y: number
-    isAccept: boolean
-    text: string
+    x: number;
+    y: number;
+    isAccept: boolean;
+    text: string;
 }
 
 export type SerializedTransition = {
-    id: string
-    from: string
-    to: string
-    transition: string[]
-    parallelPart: number
-    perpendicularPart: number
-    lineAngleAdjust: number
+    id: string;
+    from: string;
+    to: string;
+    transition: string[];
+    parallelPart: number;
+    perpendicularPart: number;
+    lineAngleAdjust: number;
 }
