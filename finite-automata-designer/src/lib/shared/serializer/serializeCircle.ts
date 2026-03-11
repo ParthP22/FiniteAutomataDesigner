3578c41ead942d9975085f6b8f636a10b215f064
ParthP22 <parthpatel0422@gmail.com>
2026-03-11 15:22:33 -0400

    refactor: updated the types in serializeCircle.ts after the recent change in names

--- finite-automata-designer/src/lib/shared/serializer/serializeCircle.ts
+++ finite-automata-designer/src/lib/shared/serializer/serializeCircle.ts
@@ -1,7 +1,7 @@
 import { Circle } from "../../../../public/scripts/Shapes/Circle";
-import { SerializedState } from "../types";
+import { SerializedCircle } from "../types";
 
-export function serializeCircle(circle: Circle): SerializedState {
+export function serializeCircle(circle: Circle): SerializedCircle {
     return {
         id: circle.id,
         x: circle.x,
