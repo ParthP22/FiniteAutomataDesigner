689bf40184ef2bd1ed9967c55da5365534a01a2e
ParthP22 <parthpatel0422@gmail.com>
2026-03-10 22:11:00 -0400

    refactor: updated the serializeArrow function to be able to serialize both the regular Arrow and SelfArrow

--- finite-automata-designer/src/lib/shared/serializeArrow.ts
+++ finite-automata-designer/src/lib/shared/serializeArrow.ts
@@ -1,14 +1,27 @@
 import { Arrow } from "../../../public/scripts/Shapes/Arrow";
+import { SelfArrow } from "../../../public/scripts/Shapes/SelfArrow";
 import { SerializedTransition } from "./types";
 
-export function serializeArrow(arrow: Arrow): SerializedTransition{
-    return {
+export function serializeArrow(arrow: Arrow | SelfArrow): SerializedTransition{
+    const base = {
         id: arrow.id,
         from: arrow.startCircle.id,
         to: arrow.endCircle.id,
-        transition: Array.from(arrow.transition),
-        parallelPart: arrow.parallelPart,
-        perpendicularPart: arrow.perpendicularPart,
-        lineAngleAdjust: arrow.lineAngleAdjust
+        transition: Array.from(arrow.transition)
+    }
+    
+    if(arrow instanceof Arrow){
+        return {
+            ...base,
+            parallelPart: arrow.parallelPart,
+            perpendicularPart: arrow.perpendicularPart,
+            lineAngleAdjust: arrow.lineAngleAdjust
+        }
+    }
+    else{
+        return {
+            ...base,
+            anchorAngle: arrow.anchorAngle
+        }
     }
 }
