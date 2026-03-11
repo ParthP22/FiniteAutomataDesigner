e40e054fbf918eddf53c6c1ed4548fc08dc89ba9
ParthP22 <parthpatel0422@gmail.com>
2026-03-11 15:25:25 -0400

    feat: created deserializeDFA.ts with a function to deserialize a SerializedDFA and return an array of Circles, another array of Arrows, an EntryArrow, and the alphabet Set

---
+++ finite-automata-designer/src/lib/dfa/deserializeDFA.ts
@@ -0,0 +1,65 @@
+import { SerializedDFA } from "./types"
+import { deserializeCircle } from "../shared/deserializer/deserializeCircle"
+import { deserializeArrow } from "../shared/deserializer/deserializeArrow"
+import { deserializeEntryArrow } from "../shared/deserializer/deserializeEntryArrow"
+import { Circle } from "../../../public/scripts/Shapes/Circle"
+import { Arrow } from "../../../public/scripts/Shapes/Arrow"
+import { EntryArrow } from "../../../public/scripts/Shapes/EntryArrow"
+
+export type DFAObjects = {
+  circles: Circle[]
+  arrows: Arrow[]
+  entryArrow: EntryArrow
+  alphabet: Set<string>
+}
+
+export function deserializeDFA(data: SerializedDFA): DFAObjects {
+
+  const circles: Circle[] = []
+  const arrows: Arrow[] = []
+
+  const circleMap = new Map<string, Circle>()
+
+  /*
+  Step 1: Create circles
+  */
+
+  for (const c of data.circles) {
+    const circle = deserializeCircle(c)
+
+    circles.push(circle)
+    circleMap.set(c.id, circle)
+  }
+
+  /*
+  Step 2: Create arrows
+  */
+
+  for (const a of data.arrows) {
+    const arrow = deserializeArrow(a, circleMap)
+
+    arrows.push(arrow)
+
+    // reconnect circle adjacency
+    arrow.startCircle.outArrows.add(arrow)
+  }
+
+  /*
+  Step 3: Entry Arrow
+  */
+
+  const entryArrow = deserializeEntryArrow(data.entryArrow, circleMap)
+
+  /*
+  Step 4: Alphabet
+  */
+
+  const alphabet = new Set(data.alphabet)
+
+  return {
+    circles,
+    arrows,
+    entryArrow,
+    alphabet
+  }
+}
