d45fe738a46b84ebe948c44f5fecea9affeba881
ParthP22 <parthpatel0422@gmail.com>
2026-03-11 15:23:25 -0400

    refactor: updated the types in serializeDFA.ts after the recent change in names

--- finite-automata-designer/src/lib/dfa/serializeDFA.ts
+++ finite-automata-designer/src/lib/dfa/serializeDFA.ts
@@ -1,5 +1,5 @@
-import { serializeCircle } from "../shared/serializeCircle";
-import { serializeArrow } from "../shared/serializeArrow";
+import { serializeCircle } from "../shared/serializer/serializeCircle";
+import { serializeArrow } from "../shared/serializer/serializeArrow";
 import { SerializedDFA } from "./types";
 import { Circle } from "../../../public/scripts/Shapes/Circle";
 import { Arrow } from "../../../public/scripts/Shapes/Arrow";
@@ -8,14 +8,14 @@ import { alphabet } from "../../../public/scripts/alphabet";
 import { startState } from "../../../public/scripts/Shapes/EntryArrow";
 import { arrows } from "../../../public/scripts/Shapes/Arrow";
 import { circles } from "../../../public/scripts/Shapes/Circle";
-import { serializeEntryArrow } from "../shared/serializeEntryArrow";
+import { serializeEntryArrow } from "../shared/serializer/serializeEntryArrow";
 
 export function serializeDFA(
 ): SerializedDFA {
     return {
         alphabet: Array.from(alphabet),
-        states: circles.map(serializeCircle),
-        transitions: arrows.map(serializeArrow),
+        circles: circles.map(serializeCircle),
+        arrows: arrows.map(serializeArrow),
         entryArrow: serializeEntryArrow(startState)
     }
 }
