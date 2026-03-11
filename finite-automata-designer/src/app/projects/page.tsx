8927f2aa4464c083acc67e7a7a3eabe4f65f8b00
ParthP22 <parthpatel0422@gmail.com>
2026-03-11 17:49:56 -0400

    feat: created projects/page.tsx page, where all the saved machines of the user are listed. It's not fully functional yet: when you click a machine, it takes you to an editor which doesn't fully load in yet. This will be implemented later today hoopefully

---
+++ finite-automata-designer/src/app/projects/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { createClient } from "@/lib/supabase/client";
+
+export default function AutomataPage() {
+  const [machines, setMachines] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function loadMachines() {
+      const supabase = createClient();
+
+      const { data: { user } } = await supabase.auth.getUser();
+      if (!user) return;
+
+      const { data, error } = await supabase
+        .from("finite_automata")
+        .select("*")
+        .eq("user_id", user.id);
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      setMachines(data || []);
+    }
+
+    loadMachines();
+  }, []);
+
+  return (
+    <main className="min-h-screen bg-blue-100 p-10 text-black">
+      <h1 className="text-4xl font-bold mb-6 text-center">My Projects</h1>
+
+      <div className="flex flex-col gap-4">
+        {machines.map((machine) => (
+          <Link
+            key={machine.id}
+            href={`/projects/${machine.id}`}
+            className="bg-gray-700 text-white px-4 py-3 rounded hover:bg-black transition"
+          >
+            Automaton {machine.id}
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
