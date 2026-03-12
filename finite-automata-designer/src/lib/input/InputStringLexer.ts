b9c5d2749f6700c627cfd17a8da10b90b31e3e4f
ParthP22 <parthpatel0422@gmail.com>
2026-03-12 13:01:40 -0400

    refactor: since we have different transitionLabelInputValidators for the DFA and NFA, I updated parseInputString to pass in the transitionLabelInputValidator as a parameter.

--- finite-automata-designer/src/lib/input/InputStringLexer.ts
+++ finite-automata-designer/src/lib/input/InputStringLexer.ts
@@ -1,7 +1,8 @@
-import { transitionLabelInputValidator } from "../../../public/scripts/alphabet";
-
 // A discriminated union type that represents either:
 // 1. A successful parse with an array of tokens
+
+import { TransitionLabelInputValidator } from "../validation/TransitionLabelInputValidator";
+
 // 2. A failed parse with an error message
 export type ParseResult =
     | { success: true; tokens: string[] }
@@ -11,7 +12,8 @@ export type ParseResult =
 // tokens according to the alphabet and delimiter rules.
 export function parseInputString(
     input: string,
-    alphabet: Set<string>
+    alphabet: Set<string>,
+    transitionLabelInputValidator: TransitionLabelInputValidator
 ): ParseResult {
 
     // Remove leading and trailing whitespace from the input
