2f91b579e215fd1962a5e98a98cd9f9a1ec5c06c
ParthP22 <parthpatel0422@gmail.com>
2026-03-12 22:53:49 -0400

    fix: instead of serializing the DFA based on the array imports, I am now passing them in as parameters into the serializeDFA function, which now works, since I'm actually passing in non-empty arrays.

--- finite-automata-designer/src/lib/dfa/serializeDFA.ts
+++ finite-automata-designer/src/lib/dfa/serializeDFA.ts
@@ -1,14 +1,20 @@
 import { serializeCircle } from "../shared/serializer/serializeCircle";
 import { serializeArrow } from "../shared/serializer/serializeArrow";
 import { SerializedDFA } from "./types";
-import { alphabet } from "./dfaTransitionSymbols";
-import { startState } from "../../../public/scripts/Shapes/EntryArrow";
-import { arrows } from "../../../public/scripts/Shapes/Arrow";
-import { circles } from "../../../public/scripts/Shapes/Circle";
+import { EntryArrow } from "../../../public/scripts/Shapes/EntryArrow";
+import { Arrow } from "../../../public/scripts/Shapes/Arrow";
+import { Circle } from "../../../public/scripts/Shapes/Circle";
 import { serializeEntryArrow } from "../shared/serializer/serializeEntryArrow";
+import { SelfArrow } from "../../../public/scripts/Shapes/SelfArrow";
 
 export function serializeDFA(
+    alphabet: Set<string>,
+    circles: Circle[],
+    arrows: (Arrow | SelfArrow)[],
+    startState: EntryArrow | null
 ): SerializedDFA {
+    console.log(circles);
+    console.log(circles.map(serializeCircle));
     return {
         alphabet: Array.from(alphabet),
         circles: circles.map(serializeCircle),
