5dd20e8595b13d4a7e0ecf29dd9d092f76d51bde
ParthP22 <parthpatel0422@gmail.com>
2026-03-12 23:24:37 -0400

    fix: I forgot to implement the UI of the Arrow and SelfArrow when deserializing, so any Arrows with curvature didn't actually have curvature when loaded. So, that has now been fixed

--- finite-automata-designer/src/lib/shared/deserializer/deserializeArrow.ts
+++ finite-automata-designer/src/lib/shared/deserializer/deserializeArrow.ts
@@ -1,11 +1,12 @@
 import { Arrow } from "../../../../public/scripts/Shapes/Arrow"
 import { SerializedArrow } from "../types"
 import { Circle } from "../../../../public/scripts/Shapes/Circle"
+import { SelfArrow } from "../../../../public/scripts/Shapes/SelfArrow";
 
 export function deserializeArrow(
   data: SerializedArrow,
   circleMap: Map<string, Circle>
-): Arrow {
+): Arrow | SelfArrow{
 
   const fromCircle = circleMap.get(data.from);
   const toCircle = circleMap.get(data.to);
@@ -14,10 +15,25 @@ export function deserializeArrow(
     throw new Error("Arrow references missing circle");
   }
 
-  const arrow = new Arrow(fromCircle, toCircle);
+
+  const arrow = 
+    (fromCircle === toCircle) ? 
+    new SelfArrow(fromCircle, toCircle) : 
+    new Arrow(fromCircle, toCircle);
 
   arrow.id = data.id;
   arrow.text = data.transition.join(",");
+  arrow.transition = new Set<string>(data.transition);
+
+  if(arrow instanceof Arrow){
+    arrow.lineAngleAdjust = data.lineAngleAdjust!;
+    arrow.parallelPart = data.parallelPart!;
+    arrow.perpendicularPart = data.perpendicularPart!;
+  }
+  if(arrow instanceof SelfArrow){
+    arrow.anchorAngle = data.anchorAngle!;
+  }
+  
 
   return arrow;
 }
