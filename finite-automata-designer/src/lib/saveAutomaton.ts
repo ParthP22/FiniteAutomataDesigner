a434f2b4c64717c151fbfce7dfe54f65a3e19de1
ParthP22 <parthpatel0422@gmail.com>
2026-06-13 14:31:19 -0400

    refactor: save the automata as a CreateAutomaton interface when adding to DB

--- finite-automata-designer/src/lib/saveAutomaton.ts
+++ finite-automata-designer/src/lib/saveAutomaton.ts
@@ -1,16 +1,17 @@
+import { SerializedDFA } from "./dfa/types";
+import { CreateAutomaton } from "./shared/types";
 import { createClient } from "./supabase/client";
 
-export async function saveAutomaton(userId: string, serializedDFA: unknown){
+export async function saveAutomaton(userId: string, serializedDFA: SerializedDFA){
     const supabase = createClient();
 
     const { data, error } = await supabase
         .from("finite_automata")
         .insert({
             user_id: userId,
-            name: "Untitled DFA",
             type: "DFA",
             automaton: serializedDFA
-        })
+        } as CreateAutomaton)
         .select()
         .single();
 
