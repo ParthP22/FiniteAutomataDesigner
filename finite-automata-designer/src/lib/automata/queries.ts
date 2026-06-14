b17d8a16d8f62c969438305d9a3bbb11b2f83aaa
ParthP22 <parthpatel0422@gmail.com>
2026-06-14 15:17:51 -0400

    feat: created getAutomaton function

--- finite-automata-designer/src/lib/automata/queries.ts
+++ finite-automata-designer/src/lib/automata/queries.ts
@@ -16,11 +16,34 @@ export async function getUserAutomata(){
         .eq("user_id", user.id);
 
 
-    if (error) {
+    if (!data || error) {
         console.error(error);
         throw new Error("There was an error fetching the user's projects.");
     }
 
     return data as FiniteAutomaton[];
     
+}
+
+export async function getAutomaton(automatonId: string){
+    const supabase = createClient();
+
+    const { data: { user }, error: userError} = await supabase.auth.getUser();
+
+    if(!user || userError){
+        throw new Error("User is not authenticated.");
+    }
+
+    const { data, error } = await supabase
+        .from("finite_automata")
+        .select("*")
+        .eq("id", automatonId)
+        .single();
+
+    if(!data || error){
+        console.error(error);
+        throw new Error("Failed to fetch selected automaton.");
+    }
+
+    return data as FiniteAutomaton;
 }
