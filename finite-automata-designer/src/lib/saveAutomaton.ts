6880efabd31498b3bd9fe8c7ce54904134bc085a
ParthP22 <parthpatel0422@gmail.com>
2026-06-16 16:05:15 -0400

    feat: added name and description fields to saveAutomaton function, and also added authentication check

--- finite-automata-designer/src/lib/saveAutomaton.ts
+++ finite-automata-designer/src/lib/saveAutomaton.ts
@@ -2,13 +2,22 @@ import { SerializedDFA } from "./dfa/types";
 import { CreateAutomaton } from "./shared/types";
 import { createClient } from "./supabase/client";
 
-export async function saveAutomaton(userId: string, serializedDFA: SerializedDFA){
+export async function saveAutomaton(serializedDFA: SerializedDFA, name: string | null, description: string | null){
     const supabase = createClient();
+
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if(!user){
+        alert("You must be logged in to save.");
+        return;
+    }
 
     const { data, error } = await supabase
         .from("finite_automata")
         .insert({
-            user_id: userId,
+            user_id: user.id,
+            name: name,
+            description: description,
             type: "DFA",
             automaton: serializedDFA
         } as CreateAutomaton)
