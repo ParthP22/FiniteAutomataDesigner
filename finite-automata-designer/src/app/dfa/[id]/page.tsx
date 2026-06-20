bfa4ea1495c1e9b8dae753bb062ac53b3027420a
ParthP22 <parthpatel0422@gmail.com>
2026-06-20 14:18:38 -0400

    refactor: added ImportTextArea component to editor pages

--- finite-automata-designer/src/app/dfa/[id]/page.tsx
+++ finite-automata-designer/src/app/dfa/[id]/page.tsx
@@ -151,18 +151,7 @@ function DFAPageContent() {
                     
                     <ExportTextArea />
 
-                    <div id="importInputContainer" hidden className="text-center">
-                        <textarea 
-                        tabIndex={-1}
-                        id="input"
-                        className="w-full h-24 mt-2 border border-gray-400 rounded text-black bg-white select-none" 
-                        placeholder="Enter the input data that you exported EXCLUSIVELY from this FA designer goes here...">
-                        </textarea>
-                        <div className="flex gap-4 justify-center text-black">
-                            <button id='hideInput' type="button" className="cursor-pointer text-center text-decoration underline">Hide Input</button>
-                            <button id='clearInput' type="button" className="cursor-pointer text-center text-decoration underline">Clear Input</button>
-                        </div>
-                    </div>
+                    
                 
                 </div>
             </div>
