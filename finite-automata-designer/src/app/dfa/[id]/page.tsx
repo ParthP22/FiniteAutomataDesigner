8991eb52d0e71584f14bcae7c31a5d77a4379319
ParthP22 <parthpatel0422@gmail.com>
2026-06-20 14:24:14 -0400

    refactor: added InputString component to editor pages

--- finite-automata-designer/src/app/dfa/[id]/page.tsx
+++ finite-automata-designer/src/app/dfa/[id]/page.tsx
@@ -12,6 +12,7 @@ import AutomataHeader from "@/app/components/editor/AutomataHeader";
 import ImportContainer from "@/app/components/editor/import/ImportContainer";
 import ExportContainer from "@/app/components/editor/export/ExportContainer";
 import ExportTextArea from "@/app/components/editor/export/ExportTextArea";
+import InputString from "@/app/components/editor/InputString";
 
 
 function DFAPageContent() {
@@ -162,21 +163,8 @@ function DFAPageContent() {
                     <div className="flex flex-col gap-5">
                         <div id='inputDiv' className="flex flex-col self-center w-full max-w-md text-black">
                             {/* Textbox for inputting strings */}
-                            <label htmlFor="inputString" className="block mb-1 text-gray-700 text-xl font-bold">
-                                Input:
-                            </label>
-                            <input
-                                id="inputString"
-                                type="text"
-                                placeholder="Enter a string..."
-                                className="w-full px-4 py-2 border border-gray-400 rounded shadow-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-400"
-                                // Loose focus after you press enter
-                                onKeyDown={(e) => {
-                                    if (e.key === "Enter") {
-                                        e.currentTarget.blur();
-                                    }
-                                }}
-                            />
+                            <InputString />
+
                             <div className="min-h-[3rem]">
                                 {hasMultiCharAlphabet && (
                                     <div className="w-full rounded border border-red-300 bg-red-100 px-3 py-2">
