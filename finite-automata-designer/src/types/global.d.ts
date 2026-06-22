6596812e2f18a5fbe2abba724dbf6f1293e77aae
ParthP22 <parthpatel0422@gmail.com>
2026-06-22 17:47:31 -0400

    feat: added loadNFAIntoCanvas and exportNFA attributes to the global Window interface

--- finite-automata-designer/src/types/global.d.ts
+++ finite-automata-designer/src/types/global.d.ts
@@ -4,8 +4,14 @@ declare global {
   interface Window {
     /** Loads a serialized automaton object into the DFA canvas. */
     loadDFAIntoCanvas: (automaton: SerializedDFA) => void;
+
+    /** Loads a serialized automaton object into the NFA canvas. */
+    loadNFAIntoCanvas: (automaton: SerializedNFA) => void;
     
     /** Serializes the current DFA canvas state and returns it. */
     exportDFA: () => SerializedDFA;
+
+    /** Serializes the current NFA canvas state and returns it. */
+    exportNFA: () => SerializedNFA;
   }
 }
