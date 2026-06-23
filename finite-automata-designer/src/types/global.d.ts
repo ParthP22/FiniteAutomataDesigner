fbef76ee72b66b726432e370d7ca0c4de0b9ecd3
ParthP22 <parthpatel0422@gmail.com>
2026-06-23 15:23:57 -0400

    refactor: created loadFAIntoCanvas and exportFA, and deleted the ones for DFA and NFA in global.d.ts

--- finite-automata-designer/src/types/global.d.ts
+++ finite-automata-designer/src/types/global.d.ts
@@ -2,16 +2,11 @@ export {};
 
 declare global {
   interface Window {
-    /** Loads a serialized automaton object into the DFA canvas. */
-    loadDFAIntoCanvas: (automaton: SerializedDFA) => void;
+    /** Loads a serialized automaton object into the FA canvas. */
+    loadFAIntoCanvas: (automaton: SerializedFA) => void;
 
-    /** Loads a serialized automaton object into the NFA canvas. */
-    loadNFAIntoCanvas: (automaton: SerializedNFA) => void;
-    
     /** Serializes the current DFA canvas state and returns it. */
-    exportDFA: () => SerializedDFA;
+    exportFA: () => SerializedFA;
 
-    /** Serializes the current NFA canvas state and returns it. */
-    exportNFA: () => SerializedNFA;
   }
 }
