9dd6612a353f819756b9d7f12abce9d212039ceb
ParthP22 <parthpatel0422@gmail.com>
2026-07-04 18:25:50 -0400

    chore: further reduced the code on the DFA and NFA pages by removing an unnecessary abstraction

--- finite-automata-designer/src/app/dfa/page.tsx
+++ finite-automata-designer/src/app/dfa/page.tsx
@@ -3,17 +3,10 @@
 import { Suspense } from "react";
 import AutomataEditor from "../components/editor/AutomataEditor";
 
-function DFAPageContent() {
-
-    return (
-        <AutomataEditor type="DFA" />
-    );
-}
-
 export default function DFAPage() {
     return (
         <Suspense>
-            <DFAPageContent />
+            <AutomataEditor type="DFA" />
         </Suspense>
     );
 }
--- finite-automata-designer/src/app/nfa/page.tsx
+++ finite-automata-designer/src/app/nfa/page.tsx
@@ -3,17 +3,10 @@
 import { Suspense } from "react";
 import AutomataEditor from "../components/editor/AutomataEditor";
 
-function NFAPageContent() {
-
-    return (
-        <AutomataEditor type="NFA" />
-    );
-}
-
 export default function NFAPage() {
     return (
         <Suspense>
-            <NFAPageContent />
+            <AutomataEditor type="NFA" />
         </Suspense>
     );
 }
